d4f7b6cddc7f2a1f51313780bcfb77453bafcc60
martinrdrz <martinrdrz@hotmail.com>
2024-07-10 14:02:13 -0300

    CRUD de eventos del calendario

--- models/Usuario.js
+++ models/Usuario.js
@@ -3,19 +3,17 @@ const { Schema, model } = require('mongoose');
 const UsuarioSchema = Schema({
     name: {
         type: String,
-        require: true
+        required: true,
     },
     email: {
         type: String,
-        require: true,
-        unique: true
+        required: true,
+        unique: true,
     },
     password: {
         type: String,
-        require: true
-    }
+        required: true,
+    },
 });
 
-
-module.exports = model('Usuario', UsuarioSchema );
-
+module.exports = model('Usuario', UsuarioSchema);
