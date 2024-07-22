7ba1fb07e8757cd246f0319c5b392434dcc4dd9f
martinrdrz <martinrdrz@hotmail.com>
2024-07-22 20:28:33 -0300

    actualizacion carpeta statis con front-end

--- routes/events.js
+++ routes/events.js
@@ -13,37 +13,36 @@ const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('.
 const router = Router();
 
 // Todas tienes que pasar por la validación del JWT
-router.use( validarJWT );
+router.use(validarJWT);
 
-
-// Obtener eventos 
-router.get('/', getEventos );
+// Obtener eventos
+router.get('/', getEventos);
 
 // Crear un nuevo evento
 router.post(
     '/',
     [
-        check('title','El titulo es obligatorio').not().isEmpty(),
-        check('start','Fecha de inicio es obligatoria').custom( isDate ),
-        check('end','Fecha de finalización es obligatoria').custom( isDate ),
-        validarCampos
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos,
     ],
-    crearEvento 
+    crearEvento
 );
 
 // Actualizar Evento
 router.put(
-    '/:id', 
+    '/:id',
     [
-        check('title','El titulo es obligatorio').not().isEmpty(),
-        check('start','Fecha de inicio es obligatoria').custom( isDate ),
-        check('end','Fecha de finalización es obligatoria').custom( isDate ),
-        validarCampos
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+        validarCampos,
     ],
-    actualizarEvento 
+    actualizarEvento
 );
 
 // Borrar evento
-router.delete('/:id', eliminarEvento );
+router.delete('/:id', eliminarEvento);
 
 module.exports = router;
